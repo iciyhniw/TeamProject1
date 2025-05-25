4683df547639ed1718d939563d24352c6ac0025f
iciyhniw <08andrey1205@gmail.com>
2025-05-25 17:58:35 +0300

    ХЗ

--- jamendo-player/src/components/Navbar.jsx
+++ jamendo-player/src/components/Navbar.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = ({ onSearch }) => (
-  <div className="navbar">
-    <input
-      type="text"
-      placeholder="Search music..."
-      onChange={(e) => onSearch(e.target.value)}
-    />
-    <div className="nav-links">
-      <Link to="/">Home</Link>
-      <Link to="/search">Search</Link>
+    <div className="navbar">
+        <div className="nav-links">
+            <Link to="/">Home</Link>
+        </div>
+        <input
+            type="text"
+            placeholder="Search music..."
+            onChange={(e) => onSearch(e.target.value)}
+        />
+        <div className="nav-links nav-links-right">
+            <Link to="/profile">Profile</Link>
+        </div>
     </div>
-  </div>
 );
 
 export default Navbar;
