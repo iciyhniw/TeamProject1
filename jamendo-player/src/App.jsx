753a088515a37df713570ed703d000238740db9d
Adwersity <maksym.dubyk1@gmail.com>
2025-05-25 19:21:49 +0300

    updates, add firebase

--- jamendo-player/src/App.jsx
+++ jamendo-player/src/App.jsx
@@ -4,9 +4,23 @@ import Home from './pages/Home';
 import Search from './pages/Search';
 import './styles/CommonFile.css'
 import Profile from "./pages/Profile";
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Navbar from './components/Navbar';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
+const App = () => {
+  const [user, setUser] = useState(null); // 🔹 Додаємо стан користувача
 
-const App = () => (
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser); // 🔄 оновлення користувача при вході/виході
+    });
+
+    return () => unsubscribe(); // очистка слухача
+  }, []);
+  return(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
