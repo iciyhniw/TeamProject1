e4b935c8536d268891101191121d574a564d6402
iciyhniw <08andrey1205@gmail.com>
2025-05-26 18:30:50 +0300

    Genrecards..

--- jamendo-player/src/components/Navbar.jsx
+++ jamendo-player/src/components/Navbar.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { FaSearch } from 'react-icons/fa';
 
 const Navbar = ({ user }) => {
     return (
         <div className="navbar">
-            <div className="nav-links">
+            <div className="nav-links nav-links-left">
                 <Link to="/">Home</Link>
-                <Link to="/library">Library</Link>
-                <Link to="/search">Search</Link>
             </div>
             <div className="nav-links nav-links-right">
+                <Link to="/search" className="search-icon" aria-label="Search">
+                    <FaSearch />
+                </Link>
+                <Link to="/library">Library</Link>
                 <Link to="/profile">Profile</Link>
                 {user ? (
                     <button onClick={() => signOut(auth)} className="nav-link-button">Вихід</button>
