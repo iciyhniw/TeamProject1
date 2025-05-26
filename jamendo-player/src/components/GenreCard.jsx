de43a470fe1e8c00cdce688d67b12b03930087a5
iciyhniw <08andrey1205@gmail.com>
2025-05-26 17:15:08 +0300

    Genrecards..

--- jamendo-player/src/components/GenreCard.jsx
+++ jamendo-player/src/components/GenreCard.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+const genreEmojis = {
+    pop: "🎤",
+    rock: "🎸",
+    jazz: "🎷",
+    hiphop: "🎧",
+    electronic: "🔊",
+    classical: "🎼",
+    reggae: "🌴"
+};
+
+const genreGradients = {
+    pop: "from-pink-400 to-purple-500",
+    rock: "from-gray-700 to-red-600",
+    jazz: "from-amber-400 to-orange-500",
+    hiphop: "from-green-400 to-blue-500",
+    electronic: "from-cyan-400 to-purple-600",
+    classical: "from-indigo-300 to-blue-400",
+    reggae: "from-green-400 to-yellow-400"
+};
+
 const GenreCard = ({ genre, onClick }) => (
-  <div className="genre-card" onClick={onClick}>
-    <h3>{genre}</h3>
-  </div>
+    <div
+        className={`genre-card bg-gradient-to-br ${genreGradients[genre] || 'from-gray-400 to-gray-600'}`}
+        onClick={onClick}
+    >
+        <div className="genre-emoji">
+            {genreEmojis[genre] || "🎵"}
+        </div>
+        <h3 className="genre-title">{genre}</h3>
+    </div>
 );
 
 export default GenreCard;
